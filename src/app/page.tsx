8f611e00a5490cb422a9c3629848e92f9d0c1f1f
Eric Dang <edang830@gmail.com>
2023-07-21 15:24:38 -0700

    Move Navbar to layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,6 @@
-import {Navbar} from '@/components/Navbar';
-
 export default function Home() {
   return (
     <main>
-      <Navbar />
       <div className="h-screen w-full bg-slate-400 flex flex-col justify-center items-center text-center">
         <div className="mb-20">
           <h1 className="text-8xl">Hello, I&apos;m Eric Dang</h1>
