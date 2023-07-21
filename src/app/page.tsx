58dc0777713c79c56d53ee911eab0485c1f3b517
Eric Dang <edang830@gmail.com>
2023-07-21 15:11:50 -0700

    Add basic home page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,15 @@
+import {Navbar} from '@/components/Navbar';
+
 export default function Home() {
-  return <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>;
+  return (
+    <main>
+      <Navbar />
+      <div className="h-screen w-full bg-slate-400 flex flex-col justify-center items-center text-center">
+        <div className="mb-20">
+          <h1 className="text-8xl">Hello, I&apos;m Eric Dang</h1>
+          <h3 className="text-3xl">Software Engineer</h3>
+        </div>
+      </div>
+    </main>
+  );
 }
