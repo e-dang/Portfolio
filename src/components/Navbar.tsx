8969d0d5eee048d5f74575d77463c68d2222e65c
Eric Dang <edang830@gmail.com>
2023-07-21 15:11:31 -0700

    Update navbar styling

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,15 +1,24 @@
 import Link from 'next/link';
+import './Navbar.css';
 
 export const Navbar = () => {
   return (
-    <nav className="flex justify-end bg-slate-400 p-4">
-      <Link className="mx-2" href="/projects">
-        Projects
-      </Link>
+    <nav className="fixed top-0  bg-slate-400 w-full p-4 fade-in-from-top">
+      <div className="container mx-auto">
+        <div className="flex items-center justify-between">
+          <Link href="/">Eric Dang</Link>
 
-      <Link className="mx-2" href="/experience">
-        Experience
-      </Link>
+          <ul className="flex space-x-4">
+            <li>
+              <Link href="/projects">Projects</Link>
+            </li>
+
+            <li>
+              <Link href="/experience">Experience</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
     </nav>
   );
 };
