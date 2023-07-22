9ecb7f552f6b4d1c170b07d4e6e6966177858930
Eric Dang <edang830@gmail.com>
2023-07-22 12:50:49 -0700

    Add prettier

---
+++ .prettierrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  plugins: [require('prettier-plugin-tailwindcss')],
+  bracketSpacing: false,
+  bracketSameLine: false,
+  singleQuote: true,
+  trailingComma: 'all',
+  arrowParens: 'always',
+  embeddedLanguageFormatting: 'auto',
+  htmlWhitespaceSensitivity: 'css',
+  insertPragma: false,
+  printWidth: 120,
+  proseWrap: 'preserve',
+  quoteProps: 'as-needed',
+  requirePragma: false,
+  semi: true,
+  tabWidth: 2,
+  useTabs: false,
+  overrides: [
+    {
+      files: ['*.yaml', '*.yml'],
+      options: {
+        proseWrap: 'always',
+      },
+    },
+  ],
+};
