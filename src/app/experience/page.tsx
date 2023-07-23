a18655b82f815d8ae6333f02c52c75c8712778ce
Eric Dang <edang830@gmail.com>
2023-07-23 16:21:54 -0700

    Add extra height for scrolling to bottom

--- src/app/experience/page.tsx
+++ src/app/experience/page.tsx
@@ -16,7 +16,7 @@ export default function Experience() {
         <div className="relative before:absolute before:bottom-0 before:left-1/4 before:top-1 before:z-10 before:w-2 before:bg-black before:content-['']">
           <div className="sticky left-1/4 top-32 z-20 w-1/2">
             <div className="relative -left-8 flex flex-shrink">
-              <h1 className="rounded-full border-8 border-black bg-white p-6 text-6xl">Work Experience</h1>
+              <h1 className=" border-b-8 border-black bg-white p-6 text-6xl">Work Experience</h1>
             </div>
           </div>
 
@@ -40,16 +40,24 @@ export default function Experience() {
             </div>
           </div>
 
-          <div className="relative left-1/4 my-40 w-1/2">
+          <div className="relative left-1/4 mt-40 w-1/2">
             <div className="sticky top-[26rem]">
               <div className="absolute -left-4 h-0 w-0 border-x-[20px] border-y-4 border-solid border-black"></div>
             </div>
 
             <div className="relative left-1/4">
               <ExperienceCard />
+            </div>
+          </div>
+
+          <div className="relative left-1/4 w-1/2">
+            <div className="sticky top-[26rem]">
+              <div className="absolute -left-4 h-0 w-0 border-x-[20px] border-y-4 border-solid border-black"></div>
             </div>
           </div>
         </div>
+
+        <div className="h-[992px]" />
       </main>
     </div>
   );
