b46e34876e697f7f6c72c50b501a1d7adc31e214
Eric Dang <edang830@gmail.com>
2023-08-05 13:30:43 -0700

    Add content for experience page

--- src/app/experience/page.tsx
+++ src/app/experience/page.tsx
@@ -21,7 +21,25 @@ export default function Experience() {
             </div>
 
             <div className="relative left-1/4">
-              <ExperienceCard />
+              <ExperienceCard
+                title="Software Engineer"
+                company="Fulgent Genetics"
+                summary="Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim provident ullam veniam consequatur praesentium earum qui eius assumenda, ducimus aliquam distinctio aperiam aliquid vel molestiae, magnam nemo voluptatem labore neque."
+                achievements={[
+                  'Developed and deployed the user interface of the Fulgent Oncology Provider Portal, facilitating healthcare providers in managing patient information, orders, and accessing test results and requisition forms.',
+
+                  'Developed and deployed the Fulgent Oncology Admin Portal user interface, empowering internal Fulgent employees to enroll healthcare providers in Fulgent Oncology services, and efficiently access customer data required to carry out various customer support tasks.',
+
+                  'Developed and deployed a backend service that enables Fulgent Genetics&apos; frontend services to efficiently search for valid ICD-10 codes. Additionally, the service allows for Fulgent admins to upload new ICD-10 codes as they are released.',
+
+                  'Deployed and actively managed CI pipelines, compute infrastructure (EKS), and IAM policies and roles on AWS using Terraform, enabling tracking of architectural changes in code for better code reuse and easily reproducible deployments.',
+
+                  'Automated the formerly manual process of deployment using FluxCD, a GitOps style of Continuous Deployment, on staging and production EKS clusters.',
+
+                  'Decoupled the Fulgent Pharma site from a monolithic app previously deployed on bare EC2 instances into Dockerized microservices deployed on Kubernetes (EKS), improving scalability, availability, resource utilization, and automation.',
+                ]}
+                technologies={['Typescript', 'Kubernetes', 'Docker', 'Node.JS', 'Vue.JS']}
+              />
             </div>
           </div>
 
@@ -31,17 +49,19 @@ export default function Experience() {
             </div>
 
             <div className="relative left-1/4">
-              <ExperienceCard />
-            </div>
-          </div>
+              <ExperienceCard
+                title="Software Engineer"
+                company="University of California, Los Angeles"
+                summary="Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim provident ullam veniam consequatur praesentium earum qui eius assumenda, ducimus aliquam distinctio aperiam aliquid vel molestiae, magnam nemo voluptatem labore neque."
+                achievements={[
+                  'Created novel software (CPMG) for generating a giga-scale virtual library consisting of 2D structure macrocycle drug candidates for high throughput screening.',
 
-          <div className="relative left-1/4 mt-40 w-1/2">
-            <div className="sticky top-[26rem]">
-              <div className="absolute -left-4 h-0 w-0 border-x-[20px] border-y-4 border-solid border-black"></div>
-            </div>
+                  'Improved software for generating 3D conformers for macrocyclic compounds by decreasing runtimes by 98% using the framework RDKit to perform operations on the molecular graphs (ConfBuster++).',
 
-            <div className="relative left-1/4">
-              <ExperienceCard />
+                  'Together, CPMG and ConfBuster++ resulted in an academic publication as well as a Python package available on Anaconda cloud (https://anaconda.org/e-dang/confbusterplusplus).',
+                ]}
+                technologies={['Python', 'RDKit']}
+              />
             </div>
           </div>
 
