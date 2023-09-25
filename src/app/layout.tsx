20242ec6d99d36c98d618b9b0abaae06476200bc
Eric Dang <edang830@gmail.com>
2023-09-24 17:55:41 -0700

    Add background image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import {Navbar} from '@/components/Navbar';
 import './globals.css';
 import type {Metadata} from 'next';
 import {Inter} from 'next/font/google';
+import {Background} from '@/components/Background';
 
 const inter = Inter({subsets: ['latin']});
 
@@ -14,6 +15,7 @@ export default function RootLayout({children}: {children: React.ReactNode}) {
   return (
     <html lang="en" className="scroll-smooth bg-neutral-900 text-white">
       <body className={`${inter.className} h-screen w-screen overflow-y-auto overflow-x-hidden`}>
+        <Background />
         <Navbar />
         {children}
       </body>
---
+++ src/components/Background.tsx
@@ -0,0 +1,3 @@
+export const Background = () => {
+  return <div className="absolute -z-50 h-screen w-screen bg-[url('/background.svg')] opacity-90"></div>;
+};
