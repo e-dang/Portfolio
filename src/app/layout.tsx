0caff44fc77d9ae357bd1afaa450f3c9490ae368
Eric Dang <edang830@gmail.com>
2023-10-01 16:57:49 -0700

    Set the background on html tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import {Navbar} from '@/components/Navbar';
 import './globals.css';
 import type {Metadata} from 'next';
 import {Inter} from 'next/font/google';
-import {Background} from '@/components/Background';
 
 const inter = Inter({subsets: ['latin']});
 
@@ -13,9 +12,8 @@ export const metadata: Metadata = {
 
 export default function RootLayout({children}: {children: React.ReactNode}) {
   return (
-    <html lang="en" className="scroll-smooth bg-neutral-900 text-white">
+    <html lang="en" className="scroll-smooth bg-[url('/background.svg')] bg-fixed text-white ">
       <body className={`${inter.className} h-screen w-screen overflow-y-auto overflow-x-hidden`}>
-        <Background />
         <Navbar />
         {children}
       </body>
--- src/components/Background.tsx
+++
@@ -1,3 +0,0 @@
-export const Background = () => {
-  return <div className="absolute -z-50 h-screen w-screen bg-[url('/background.svg')] opacity-90"></div>;
-};
