d2d843504453ac290d685f0cd1ee44cdfdb16f87
Eric Dang <edang830@gmail.com>
2023-10-05 14:48:30 -0700

    Add page links to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import Link from 'next/link';
+
 export default function Home() {
   return (
     <main>
@@ -9,9 +11,22 @@ export default function Home() {
               WebkitMaskImage: 'radial-gradient(ellipse, rgba(0, 0, 0, 1) 40%, rgba(0, 0, 0, 0) 100%)',
               maskImage: 'radial-gradient(ellipse, rgba(0, 0, 0, 1) 40%, rgba(0, 0, 0, 0) 100%)',
             }}
-          ></div>
-          <h1 className="text-8xl">Hello, I&apos;m Eric Dang</h1>
-          <h3 className="text-3xl">Software Engineer</h3>
+          />
+          <div className="flex flex-col gap-4">
+            <h1 className="text-8xl">Hello, I&apos;m Eric Dang</h1>
+            <h3 className="text-3xl">Software Engineer</h3>
+            <div className="flex flex-row justify-center gap-4 pt-4">
+              <Link className="rounded-lg border-4 border-purple-500 p-4 text-xl hover:bg-purple-500" href="/projects">
+                Projects
+              </Link>
+              <Link
+                className="rounded-lg border-4 border-purple-500 p-4 text-xl hover:bg-purple-500"
+                href="/experience"
+              >
+                Experience
+              </Link>
+            </div>
+          </div>
         </div>
       </div>
     </main>
