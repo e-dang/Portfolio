148cefd27d201f690ec4ced51358114537cd9dec
Eric Dang <edang830@gmail.com>
2023-10-05 13:43:30 -0700

    Update experience page

--- src/app/experience/page.tsx
+++ src/app/experience/page.tsx
@@ -3,77 +3,66 @@ import './Experience.css';
 
 export default function Experience() {
   return (
-    <>
-      <div className="mask fixed top-0 z-10 h-[26rem] w-full"></div>
-      <div className="fixed top-14 z-20 h-20 w-full bg-neutral-800"></div>
+    <main className="mx-auto max-w-screen-xl px-12 pt-32">
+      <div className="grid grid-cols-[10rem_5rem_5rem_1fr] grid-rows-[auto_auto] gap-x-2 pb-24">
+        <div className="relative col-start-3 row-span-full before:absolute before:bottom-10 before:top-[3.75rem] before:w-[0.2rem] before:bg-purple-700 before:content-['']"></div>
 
-      <main className="mx-auto mt-32 max-w-screen-xl">
-        <div className="relative before:absolute before:bottom-0 before:left-1/4 before:top-1 before:z-10 before:w-2 before:bg-black before:content-['']">
-          <div className="sticky left-1/4 top-32 z-20 w-1/2">
-            <div className="relative -left-8 flex flex-shrink">
-              <h1 className="border-b-8 border-black bg-neutral-800 p-6 text-6xl">Work Experience</h1>
-            </div>
-          </div>
-
-          <div className="relative left-1/4 my-40 w-1/2">
-            <div className="sticky top-[26rem]">
-              <div className="absolute -left-4 h-0 w-0 border-x-[20px] border-y-4 border-solid border-black"></div>
-            </div>
-
-            <div className="relative left-1/4">
-              <ExperienceCard
-                title="Software Engineer"
-                company="Fulgent Genetics"
-                summary="Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim provident ullam veniam consequatur praesentium earum qui eius assumenda, ducimus aliquam distinctio aperiam aliquid vel molestiae, magnam nemo voluptatem labore neque."
-                achievements={[
-                  'Developed and deployed the user interface of the Fulgent Oncology Provider Portal, facilitating healthcare providers in managing patient information, orders, and accessing test results and requisition forms.',
-
-                  'Developed and deployed the Fulgent Oncology Admin Portal user interface, empowering internal Fulgent employees to enroll healthcare providers in Fulgent Oncology services, and efficiently access customer data required to carry out various customer support tasks.',
-
-                  'Developed and deployed a backend service that enables Fulgent Genetics&apos; frontend services to efficiently search for valid ICD-10 codes. Additionally, the service allows for Fulgent admins to upload new ICD-10 codes as they are released.',
-
-                  'Deployed and actively managed CI pipelines, compute infrastructure (EKS), and IAM policies and roles on AWS using Terraform, enabling tracking of architectural changes in code for better code reuse and easily reproducible deployments.',
-
-                  'Automated the formerly manual process of deployment using FluxCD, a GitOps style of Continuous Deployment, on staging and production EKS clusters.',
-
-                  'Decoupled the Fulgent Pharma site from a monolithic app previously deployed on bare EC2 instances into Dockerized microservices deployed on Kubernetes (EKS), improving scalability, availability, resource utilization, and automation.',
-                ]}
-                technologies={['Typescript', 'Kubernetes', 'Docker', 'Node.JS', 'Vue.JS']}
-              />
-            </div>
-          </div>
-
-          <div className="relative left-1/4 my-40 w-1/2">
-            <div className="sticky top-[26rem]">
-              <div className="absolute -left-4 h-0 w-0 border-x-[20px] border-y-4 border-solid border-black"></div>
-            </div>
-
-            <div className="relative left-1/4">
-              <ExperienceCard
-                title="Software Engineer"
-                company="University of California, Los Angeles"
-                summary="Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim provident ullam veniam consequatur praesentium earum qui eius assumenda, ducimus aliquam distinctio aperiam aliquid vel molestiae, magnam nemo voluptatem labore neque."
-                achievements={[
-                  'Created novel software (CPMG) for generating a giga-scale virtual library consisting of 2D structure macrocycle drug candidates for high throughput screening.',
-
-                  'Improved software for generating 3D conformers for macrocyclic compounds by decreasing runtimes by 98% using the framework RDKit to perform operations on the molecular graphs (ConfBuster++).',
-
-                  'Together, CPMG and ConfBuster++ resulted in an academic publication as well as a Python package available on Anaconda cloud (https://anaconda.org/e-dang/confbusterplusplus).',
-                ]}
-                technologies={['Python', 'RDKit']}
-              />
-            </div>
-          </div>
-
-          <div className="relative left-1/4 w-1/2">
-            <div className="sticky top-[26rem]">
-              <div className="absolute -left-4 h-0 w-0 border-x-[20px] border-y-4 border-solid border-black"></div>
-            </div>
+        <div className="col-start-1 row-start-1 flex justify-end">
+          <div className="date-range-background relative h-min w-full before:absolute before:-bottom-10 before:-left-10 before:-right-10 before:-top-10 before:-z-10 before:rounded-full before:backdrop-blur-xl before:content-['']">
+            May 2021 - Current
           </div>
         </div>
 
-        <div className="h-[992px]" />
-      </main>
-    </>
+        <div className="relative col-start-2 row-start-1 before:absolute before:left-0 before:top-2 before:h-3 before:w-3 before:rounded-full before:border-4 before:border-purple-700 before:content-['']">
+          <div className="absolute left-1 top-[2.3rem] h-[0.2rem] w-[calc(100%+0.8rem)] rotate-[30deg] bg-purple-700 " />
+        </div>
+
+        <div className="col-start-4 row-start-1 pb-10 pt-[3.75rem]">
+          <ExperienceCard
+            header="Software Engineer @ Fulgent Genetics"
+            summary="Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim provident ullam veniam consequatur praesentium earum qui eius assumenda, ducimus aliquam distinctio aperiam aliquid vel molestiae, magnam nemo voluptatem labore neque."
+            achievements={[
+              'Developed and deployed the user interface of the Fulgent Oncology Provider Portal, facilitating healthcare providers in managing patient information, orders, and accessing test results and requisition forms.',
+
+              'Developed and deployed the Fulgent Oncology Admin Portal user interface, empowering internal Fulgent employees to enroll healthcare providers in Fulgent Oncology services, and efficiently access customer data required to carry out various customer support tasks.',
+
+              'Developed and deployed a backend service that enables Fulgent Genetics&apos; frontend services to efficiently search for valid ICD-10 codes. Additionally, the service allows for Fulgent admins to upload new ICD-10 codes as they are released.',
+
+              'Deployed and actively managed CI pipelines, compute infrastructure (EKS), and IAM policies and roles on AWS using Terraform, enabling tracking of architectural changes in code for better code reuse and easily reproducible deployments.',
+
+              'Automated the formerly manual process of deployment using FluxCD, a GitOps style of Continuous Deployment, on staging and production EKS clusters.',
+
+              'Decoupled the Fulgent Pharma site from a monolithic app previously deployed on bare EC2 instances into Dockerized microservices deployed on Kubernetes (EKS), improving scalability, availability, resource utilization, and automation.',
+            ]}
+            technologies={['Typescript', 'Kubernetes', 'Docker', 'Node.JS', 'Vue.JS']}
+          />
+        </div>
+
+        <div className="col-start-1 row-start-2 flex justify-end">
+          <div className="date-range-background relative h-min w-full before:absolute before:-bottom-10 before:-left-10 before:-right-10 before:-top-10 before:-z-10 before:rounded-full before:backdrop-blur-xl before:content-['']">
+            February 2019 - June 2020
+          </div>
+        </div>
+
+        <div className="relative col-start-2 row-start-2 before:absolute before:left-0 before:top-2 before:h-3 before:w-3 before:rounded-full before:border-4 before:border-purple-700 before:content-['']">
+          <div className="absolute left-1 top-[2.3rem] h-[0.2rem] w-[calc(100%+0.8rem)] rotate-[30deg] bg-purple-700 " />
+        </div>
+
+        <div className="col-start-4 row-start-2 pb-10 pt-[3.75rem]">
+          <ExperienceCard
+            header="Software Engineer @ University of California, Los Angeles"
+            summary="Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim provident ullam veniam consequatur praesentium earum qui eius assumenda, ducimus aliquam distinctio aperiam aliquid vel molestiae, magnam nemo voluptatem labore neque."
+            achievements={[
+              'Created novel software (CPMG) for generating a giga-scale virtual library consisting of 2D structure macrocycle drug candidates for high throughput screening.',
+
+              'Improved software for generating 3D conformers for macrocyclic compounds by decreasing runtimes by 98% using the framework RDKit to perform operations on the molecular graphs (ConfBuster++).',
+
+              'Together, CPMG and ConfBuster++ resulted in an academic publication as well as a Python package available on Anaconda cloud (https://anaconda.org/e-dang/confbusterplusplus).',
+            ]}
+            technologies={['Python', 'RDKit']}
+          />
+        </div>
+      </div>
+    </main>
   );
 }
--- src/components/ExperienceCard.tsx
+++ src/components/ExperienceCard.tsx
@@ -1,37 +1,25 @@
 import {Badge} from './Badge';
 
 interface ExperienceCardProps extends React.ComponentProps<'div'> {
-  title: string;
-  company: string;
-  summary: string;
+  header: React.ReactNode;
+  summary: React.ReactNode;
   achievements: string[];
   technologies: string[];
 }
 
-export const ExperienceCard: React.FC<ExperienceCardProps> = ({
-  title,
-  company,
-  summary,
-  achievements,
-  technologies,
-  ...props
-}) => {
+export const ExperienceCard: React.FC<ExperienceCardProps> = ({header, summary, achievements, technologies}) => {
   return (
-    <div className="rounded-lg border-2 border-purple-500 p-10" {...props}>
-      <h1 className="text-4xl">
-        {title} @ {company}
-      </h1>
+    <div className="flex flex-col gap-4 rounded bg-neutral-700/40 p-20 backdrop-blur-md hover:bg-neutral-700/60">
+      <h1 className="text-4xl">{header}</h1>
 
-      <div className="mt-8">
-        <p>{summary}</p>
-      </div>
+      <div>{summary}</div>
 
-      <div className="mt-8">
-        <ul>
+      <div>
+        <ul className="flex flex-col gap-2">
           {achievements.map((achievement, index) => (
             <li
               key={index}
-              className="relative mb-4 pl-5 before:absolute before:left-0 before:top-2 before:h-2 before:w-2 before:rounded before:border-2 before:border-solid before:border-purple-500 before:content-['']"
+              className="relative pl-5 before:absolute before:left-0 before:top-2 before:h-2 before:w-2 before:rounded before:border-2 before:border-solid before:border-purple-500 before:content-['']"
             >
               {achievement}
             </li>
@@ -39,7 +27,7 @@ export const ExperienceCard: React.FC<ExperienceCardProps> = ({
         </ul>
       </div>
 
-      <ul className="mt-8 flex flex-row flex-wrap gap-4">
+      <ul className="flex flex-row flex-wrap gap-2">
         {technologies.map((tech) => (
           <li key={tech}>
             <Badge>{tech}</Badge>
