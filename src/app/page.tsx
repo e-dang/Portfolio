2ff59741e4ed5103ed41106502114b23b8fa0644
Eric Dang <edang830@gmail.com>
2023-10-07 14:28:53 -0700

    Decrease letter spacing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,10 +13,10 @@ export default function Home() {
               maskImage: 'radial-gradient(ellipse, rgba(0, 0, 0, 1) 40%, rgba(0, 0, 0, 0) 100%)',
             }}
           />
-          <div className="flex flex-col gap-4">
-            <h1 className="text-8xl">Hello, I&apos;m Eric Dang</h1>
-            <h3 className="text-3xl">Software Engineer</h3>
-            <div className="flex flex-row justify-center gap-4 pt-4">
+          <div className="flex flex-col">
+            <h1 className="text-8xl tracking-tighter">Hello, I&apos;m Eric Dang</h1>
+            <h3 className="pt-12 text-3xl">Software Engineer</h3>
+            <div className="flex flex-row justify-center gap-4 pt-16">
               <Link
                 className="group rounded-lg border-4 border-purple-500 p-4 text-xl hover:bg-purple-500"
                 href="/projects"
