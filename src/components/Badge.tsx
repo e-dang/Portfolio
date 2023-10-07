994107590b78e687cab473de02954a9746b65c96
Eric Dang <edang830@gmail.com>
2023-10-07 16:44:01 -0700

    Make badge text tighter and color darker

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -1,6 +1,6 @@
 export const Badge: React.FC<React.ComponentProps<'div'>> = ({children, ...props}) => {
   return (
-    <div {...props} className="rounded-full bg-purple-500 px-3 py-1 hover:bg-blue-300">
+    <div {...props} className="text-md rounded-full bg-purple-600 px-3 py-1 tracking-tight text-white">
       {children}
     </div>
   );
