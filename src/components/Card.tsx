0cf12ce619028a12f1336c8a61cacb96627d7369
Eric Dang <edang830@gmail.com>
2023-12-03 17:06:11 -0800

    Shrink padding

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -6,7 +6,7 @@ export interface CardProps {
 
 export function Card({children}: CardProps) {
   return (
-    <div className="rounded bg-neutral-800/20 p-8 shadow-[inset_1px_1px_0_0_rgba(148,163,184,0.1)] backdrop-blur-lg hover:bg-neutral-800/80 sm:p-10">
+    <div className="rounded bg-neutral-800/20 p-6 shadow-[inset_1px_1px_0_0_rgba(148,163,184,0.1)] backdrop-blur-lg hover:bg-neutral-800/80 sm:p-10">
       {children}
     </div>
   );
